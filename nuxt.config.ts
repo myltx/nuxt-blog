855c61b9ff59fcdbd229cc20bf9597f459325bc6
myltx <summmayl@gmail.com>
2024-07-03 16:49:09 +0800

    docs: 调整文档

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,7 @@ export default defineNuxtConfig({
         // Theme used if `html.sepia`
         sepia: 'monokai',
       },
-      preload: ['c', 'cpp', 'java', 'javascript'],
+      preload: ['c', 'cpp', 'java', 'javascript', 'typescript', 'vue'],
     },
   },
   css: [
