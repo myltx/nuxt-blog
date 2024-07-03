d109aad8dcfef6d21777d751d5d0b4916627241f
myltx <summmayl@gmail.com>
2024-07-03 17:28:17 +0800

    fix: 修改评论地址

--- pages/p/[...post].vue
+++ pages/p/[...post].vue
@@ -14,8 +14,8 @@ const path = post.join('/')
         <doc-render :article="doc" />
         <doc-toc :toc="doc.body.toc" />
         <Giscus
-          repo="chansee97/nuxt-blog" repo-id="R_kgDOJZM7Qg" category="comments"
-          category-id="DIC_kwDOJZM7Qs4CW1GZ" mapping="title" term="Welcome to my blog!" reactions-enabled="1"
+          repo="myltx/nuxt-blog" repo-id="R_kgDOMOR4qw" category="Announcements"
+          category-id="DIC_kwDOMOR4q84Cgi_C" mapping="title" term="Welcome to my blog!" reactions-enabled="1"
           emit-metadata="1" input-position="top" theme="light_tritanopia" lang="zh-CN" loading="lazy"
         />
       </template>
