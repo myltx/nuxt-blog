0d3334b7e1ffa2c7b52464da78677c290c7c5475
myltx <summmayl@gmail.com>
2024-09-20 17:52:12 +0800

    feat: 添加测试服务

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const { data } = await useFetch('/api/hello')
+</script>
+
 <template>
   <section class="prose">
+    <div>{{ data?.message }}</div>
     <ContentDoc path="me" class="slide-enter-content" />
   </section>
 </template>
