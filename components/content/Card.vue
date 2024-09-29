b5515f0f139112763d5c8eccec76efce7b8e501d
myltx <summmayl@gmail.com>
2024-09-29 11:24:53 +0800

    feat: 添加 Card 组件

---
+++ components/content/Card.vue
@@ -0,0 +1,27 @@
+<!-- components/content/Card.vue -->
+<script setup lang="ts">
+defineProps({
+  content: {
+    type: String,
+    default: 'Card Content',
+  },
+  type: {
+    type: String,
+    default: 'default',
+  },
+})
+const bgColors: {
+  [key: string]: string
+} = {
+  warning: 'bg-yellow-100',
+  success: 'bg-green-200',
+  error: 'bg-red-200',
+  info: 'bg-blue-200',
+}
+</script>
+
+<template>
+  <div class="p-2 border bg-white dark:bg-black dark:border-gray-700 rounded" :class="bgColors[type]">
+    {{ content }}
+  </div>
+</template>
